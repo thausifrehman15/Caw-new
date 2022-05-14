4e40364d638857974a5d2eb3e38041e24ced9616
thausifrehman15 <thausifrehman.m@gmail.com>
2022-05-14 21:42:59 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,10 @@ export class AppComponent {
   randEle: number[] = [];
 
   createboard() {
-    for (var i = 0; i < 10; i++) {
-      const rand = Math.floor(Math.random() * this.width * this.height) + 1;
-      this.randEle.find((x) => x === rand) ? i-- : this.randEle.push(rand);
-    }
+    // for (var i = 0; i < 10; i++) {
+    //   const rand = Math.floor(Math.random() * this.width * this.height) + 1;
+    //   this.randEle.find((x) => x === rand) ? i++ : this.randEle.push(rand);
+    // }
     console.log(this.randEle);
   }
 }
